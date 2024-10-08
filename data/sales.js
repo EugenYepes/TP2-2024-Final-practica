b044361129102203d61bcdea30b0953c878d9f46
EugenYepes <eugenionyepes@gmail.com>
2024-10-08 00:12:28 -0300

    resolve all points

--- data/sales.js
+++ data/sales.js
@@ -1,17 +1,73 @@
+import { ObjectId } from "mongodb";
 import getConnection from "./conn.js";
 const DATABASE = "sample_supplies";
-const MOVIES = "sales";
+const SALES = "sales";
 
 async function getAllSales(pageSize, page) {
-  const connectiondb = await getConnection();
-  const sales = await connectiondb
-    .db(DATABASE)
-    .collection(MOVIES)
-    .find({})
-    .limit(pageSize)
-    .skip(pageSize * page)
-    .toArray();
-  return sales;
+	const connectiondb = await getConnection();
+	const sales = await connectiondb
+		.db(DATABASE)
+		.collection(SALES)
+		.find({})
+		.limit(pageSize)
+		.skip(pageSize * page)
+		.toArray();
+	return sales;
 }
 
-export { getAllSales };
+async function getSaleById(id) {
+	const connectiondb = await getConnection();
+	const sale = await connectiondb
+		.db(DATABASE)
+		.collection(SALES)
+		.findOne({ _id: new ObjectId(id) });
+
+	return sale;
+}
+
+async function getSalesFromLocation(pageSize, page, location) {
+	const connectiondb = await getConnection();
+	const salesFromLocation = await connectiondb
+		.db(DATABASE)
+		.collection(SALES)
+		// .find({ storeLocation: location })
+		.find({ storeLocation: { $regex: new RegExp(location, 'i') } }) // seach with case insensitive
+		.limit(pageSize)
+		.skip(pageSize * page)
+		.toArray();
+	return salesFromLocation;
+}
+
+async function getSalesWithFilters(pageSize, page, location, purchaseMethod, couponUsed) {
+	const connectiondb = await getConnection();
+	const salesFiltered = await connectiondb
+		.db(DATABASE)
+		.collection(SALES)
+		.find({
+			storeLocation: { $regex: new RegExp(location, 'i') },
+			purchaseMethod: { $regex: new RegExp(purchaseMethod, 'i') },
+			couponUsed: couponUsed === 'true' // compare to a string because, here its an string
+		})
+		.limit(pageSize)
+		.skip(pageSize * page)
+		.toArray();
+
+	return salesFiltered;
+}
+
+async function getSalesSortSatisfaction(pageSize, page) {
+	const connectiondb = await getConnection();
+	const salesSatisfactionOrder = await connectiondb
+		.db(DATABASE)
+		.collection(SALES)
+		.find({})
+		.sort({ 'customer.satisfaction': 1 })
+		.limit(pageSize)
+		.skip(pageSize * page)
+		.toArray();
+
+	return salesSatisfactionOrder;
+}
+
+
+export { getAllSales, getSaleById, getSalesFromLocation, getSalesWithFilters, getSalesSortSatisfaction };
--- routes/sales.js
+++ routes/sales.js
@@ -1,13 +1,45 @@
 import express from "express";
-import { getAllSales } from "../data/sales.js";
+import { getAllSales, getSaleById, getSalesFromLocation, getSalesWithFilters, getSalesSortSatisfaction } from "../data/sales.js";
 
 const router = express.Router();
 
-router.get("/", async (req, res) => {
-  const pageSize = req.query.pageSize ? parseInt(req.query.pageSize) : 0;
-  const page = req.query.page ? parseInt(req.query.page) : 0;
+router.get("/sortSatisfaction/", async (req, res) => {
+	const pageSize = req.query.pageSize ? parseInt(req.query.pageSize) : 0;
+	const page = req.query.page ? parseInt(req.query.page) : 0;
 
-  res.json(await getAllSales(pageSize, page));
+	res.json(await getSalesSortSatisfaction(pageSize, page));
+})
+
+router.get("/filterSale/", async (req, res) => {
+	const pageSize = req.query.pageSize ? parseInt(req.query.pageSize) : 0;
+	const page = req.query.page ? parseInt(req.query.page) : 0;
+	const location = req.query.location;
+	const purchaseMethod = req.query.purchaseMethod;
+	const couponUsed = req.query.couponUsed;
+
+	res.json(await getSalesWithFilters(pageSize, page, location, purchaseMethod, couponUsed));
+})
+
+
+router.get("/storeLocation/:location", async (req, res) => {
+	const pageSize = req.query.pageSize ? parseInt(req.query.pageSize) : 0;
+	const page = req.query.page ? parseInt(req.query.page) : 0;
+	const location = req.params.location;
+
+	res.json(await getSalesFromLocation(pageSize, page, location));
+})
+
+router.get("/:id", async (req, res) => {
+	const id = req.params.id;
+
+	res.json(await getSaleById(id));
+})
+
+router.get("/", async (req, res) => {
+	const pageSize = req.query.pageSize ? parseInt(req.query.pageSize) : 0;
+	const page = req.query.page ? parseInt(req.query.page) : 0;
+
+	res.json(await getAllSales(pageSize, page));
 });
 
 export default router;
